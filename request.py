ba3a79428adc08d2bc9dfaed282eb31f7a3115be
Samuel Mckay <89430082+Retrope13@users.noreply.github.com>
2023-09-24 18:28:15 -0600

    create get_s3_message and dum

--- request.py
+++ request.py
@@ -8,47 +8,50 @@ s3 = boto3.client('s3', region_name='us-west-2')
 
 def request_API(army):
     try:
-        response = get_s3()
-        print(response)
-        dump_s3()
-        ##See if the name that is contained in the bucket is jenny, sam, or paul and then flip the necessary card.
-        if response.lower() == 'jenny' or response.lower() == 'jenny\'s':
-            handle.handle_message('thejennymckay@gmail.com', '', '', army)
-        elif response.lower() == 'paul' or response.lower() == 'paul\'s':
-            handle.handle_message('thepaulmckay@gmail.com', '', '', army)
-        elif response.lower() == 'sam' or response.lower() == 'sam\'s':
-            handle.handle_message('sammckay31@gmail.com', '', '', army)
-            
-            
-        elif any(keyword in response.lower() for keyword in ['sam jenny', 'sam\'s jenny\'s', 'sam and jenny', 'sam\'s and jenny\'s', 'sam and jenny\'s']):
-            handle.handle_message('sammckay31@gmail.com', '', '', army)
-            handle.handle_message('thejennymckay@gmail.com', '', '', army)
-            
-        elif any(keyword in response.lower() for keyword in ['jenny sam', 'jenny\'s sam\'s', 'jenny and sam', 'jenny\'s and sam\'s', 'jenny and sam\'s']):
-            handle.handle_message('sammckay31@gmail.com', '', '', army)
-            handle.handle_message('thejennymckay@gmail.com', '', '', army)
-            
-            
-        elif any(keyword in response.lower() for keyword in ['sam paul', 'sam\'s paul\'s', 'sam and paul', 'sam\'s and paul\'s', 'sam and paul\'s']):
-            handle.handle_message('sammckay31@gmail.com', '', '', army)
-            handle.handle_message('thepaulmckay@gmail.com', '', '', army)
-            
-        elif any(keyword in response.lower() for keyword in ['paul sam', 'paul\'s sam\'s', 'paul and sam', 'paul\'s and sam\'s', 'paul and sam\'s']):
-            handle.handle_message('sammckay31@gmail.com', '', '', army)
-            handle.handle_message('thepaulmckay@gmail.com', '', '', army)
-            
-        elif any(keyword in response.lower() for keyword in ['jenny paul', 'jenny\'s paul\'s', 'jenny and paul', 'jenny\'s and paul\'s', 'jenny and paul\'s']):
-            handle.handle_message('thejennymckay@gmail.com', '', '', army)
-            handle.handle_message('thepaulmckay@gmail.com', '', '', army)
-            
-        elif any(keyword in response.lower() for keyword in ['paul jenny', 'paul\'s jenny\'s', 'paul and sam', 'paul\'s and sam\'s', 'paul and sam\'s']):
-            handle.handle_message('thejennymckay@gmail.com', '', '', army)
-            handle.handle_message('thepaulmckay@gmail.com', '', '', army)
+        name = get_s3_name()
+        print(name)
+        dump_s3_name()
         
-        elif response.lower() == 'all' or response.lower() == 'all cards':
-            handle.handle_message('sammckay31@gmail.com', '', '', army)
-            handle.handle_message('thepaulmckay@gmail.com', '', '', army)
-            handle.handle_message('thejennymckay@gmail.com', '', '', army)
+        message = get_s3_message()
+        dump_s3_message()
+        ##See if the name that is contained in the bucket is jenny, sam, or paul and then flip the necessary card.
+        if name.lower() == 'jenny' or name.lower() == 'jenny\'s':
+            handle.handle_message('thejennymckay@gmail.com', '', message, army)
+        elif name.lower() == 'paul' or name.lower() == 'paul\'s':
+            handle.handle_message('thepaulmckay@gmail.com', '', message, army)
+        elif name.lower() == 'sam' or name.lower() == 'sam\'s':
+            handle.handle_message('sammckay31@gmail.com', '', message, army)
+            
+            
+        elif any(keyword in name.lower() for keyword in ['sam jenny', 'sam\'s jenny\'s', 'sam and jenny', 'sam\'s and jenny\'s', 'sam and jenny\'s']):
+            handle.handle_message('sammckay31@gmail.com', '', message, army)
+            handle.handle_message('thejennymckay@gmail.com', '', message, army)
+            
+        elif any(keyword in name.lower() for keyword in ['jenny sam', 'jenny\'s sam\'s', 'jenny and sam', 'jenny\'s and sam\'s', 'jenny and sam\'s']):
+            handle.handle_message('sammckay31@gmail.com', '', message, army)
+            handle.handle_message('thejennymckay@gmail.com', '', message, army)
+            
+            
+        elif any(keyword in name.lower() for keyword in ['sam paul', 'sam\'s paul\'s', 'sam and paul', 'sam\'s and paul\'s', 'sam and paul\'s']):
+            handle.handle_message('sammckay31@gmail.com', '', message, army)
+            handle.handle_message('thepaulmckay@gmail.com', '', message, army)
+            
+        elif any(keyword in name.lower() for keyword in ['paul sam', 'paul\'s sam\'s', 'paul and sam', 'paul\'s and sam\'s', 'paul and sam\'s']):
+            handle.handle_message('sammckay31@gmail.com', '', message, army)
+            handle.handle_message('thepaulmckay@gmail.com', '', message, army)
+            
+        elif any(keyword in name.lower() for keyword in ['jenny paul', 'jenny\'s paul\'s', 'jenny and paul', 'jenny\'s and paul\'s', 'jenny and paul\'s']):
+            handle.handle_message('thejennymckay@gmail.com', '', message, army)
+            handle.handle_message('thepaulmckay@gmail.com', '', message, army)
+            
+        elif any(keyword in name.lower() for keyword in ['paul jenny', 'paul\'s jenny\'s', 'paul and sam', 'paul\'s and sam\'s', 'paul and sam\'s']):
+            handle.handle_message('thejennymckay@gmail.com', '', message, army)
+            handle.handle_message('thepaulmckay@gmail.com', '', message, army)
+        
+        elif name.lower() == 'all' or name.lower() == 'all cards':
+            handle.handle_message('sammckay31@gmail.com', '', message, army)
+            handle.handle_message('thepaulmckay@gmail.com', '', message, army)
+            handle.handle_message('thejennymckay@gmail.com', '', message, army)
             
         else:
             pass
@@ -58,23 +61,44 @@ def request_API(army):
     return True
 
 
-def get_s3():
+def get_s3_name():
     ## find the bucket and retrieve the name that is contained within it.
     bucket_name = 'frogflipbucket'
-    object_key = 'names/doc.txt'
-    response = s3.get_object(
+    object_key = 'content/name.txt'
+    name = s3.get_object(
         Bucket=bucket_name,
         Key=object_key
     )
     
-    data = response['Body'].read()
+    data = name['Body'].read()
     return data.decode('utf-8')
 
-
-def dump_s3():
+def dump_s3_name():
     #find the bucket and clear it to prevent an infinite loop of card flipping
     bucket_name = 'frogflipbucket'
-    object_key = 'names/doc.txt'
+    object_key = 'content/name.txt'
+    s3.put_object(
+        Bucket=bucket_name,
+        Key=object_key,
+        Body=''
+    )
+
+def get_s3_message():
+    bucket_name = 'frogflipbucket'
+    object_key = 'content/message.txt'
+    name = s3.get_object(
+        Bucket=bucket_name,
+        Key=object_key
+    )
+    
+    data = name['Body'].read()
+    return data.decode('utf-8')
+    
+
+    
+def dump_s3_message():
+    bucket_name = 'frogflipbucket'
+    object_key = 'content/message.txt'
     s3.put_object(
         Bucket=bucket_name,
         Key=object_key,
@@ -84,4 +108,4 @@ def dump_s3():
 if __name__ == "__main__":
 
      ##just for testing purposes
-    get_s3()
+    get_s3_name()
